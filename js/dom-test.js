4ae2d8778b47b90264fbf3fb4a3159fe6287ad59
Cristopher Yang <dg.cris.yang@gmail.com>
2022-02-09 19:07:52 -0300

    added buttons functions

--- js/dom-test.js
+++ js/dom-test.js
@@ -17,8 +17,16 @@ const ShoppingCart = [];
 
 const cardsCryptos = document.querySelector("#cryptoShoppingCart");
 
+// C O N S T R U C T O R
 
-//funciones
+class cryptomoneda {
+    constructor(nombre, precio, compraUsd, compraCrypto) {
+        this.nombre = nombre; // nombre de la cripto, a sacar por API
+        this.precio = parseFloat(precio); // precio de la cripto, a sacar por API
+        this.compraUsd = compraUsd;
+        this.compraCrypto = compraCrypto;
+    }
+}
 
 // F U N C I O N E S   B A S I C A S
 
@@ -32,40 +40,6 @@ const restar = (num1, num2) => {
     let resultadoResta;
     resultadoResta = num1 - num2;
     return resultadoResta; // RESTAR
-}
-
-// C R E A D O R   D E   C A R D S
-
-crearCards();
-
-function crearCards() {
-    Cryptos.forEach((crypto) => {
-        const divCrypto = document.createElement("div");
-        divCrypto.classList.add("card", "cardShadow");
-
-        const cardBody = document.createElement("div");
-        cardBody.classList.add("card-body");
-
-
-        divCrypto.innerHTML += `<div class="card-body>
-                                    <h5 class="card-title">${crypto.nombre}</h5>
-                                    <ul>
-                                        <li>
-                                        precio: ${crypto.precio}
-                                        </li>
-                                        <li>
-                                        precio: ${crypto.compraUsd}
-                                        </li>
-                                        <li>
-                                        precio: ${crypto.compraDeCrypto}
-                                        </li>
-                                    </ul>
-                                </div>`
-
-        cardsCryptos.appendChild(divCrypto);
-
-        divCrypto.appendChild(cardBody);
-    })
 }
 
 
@@ -90,6 +64,7 @@ const search = (term) => {
 const fiatSelected = document.querySelector('#monedaFiat');
 
 fiatSelected.addEventListener('change', (moneda) => {
+
     var valorDolar = plataFiat[moneda.target.value].valorDolar;
     var cantidadPesos = document.querySelector("#cantidadPesos").value;
     const cantidadDolares = dividir(cantidadPesos, valorDolar);
@@ -102,71 +77,48 @@ fiatSelected.addEventListener('change', (moneda) => {
 
 const cryptoSelected = document.querySelector('#selectCrypto');
 
-cryptoSelected.addEventListener('change', (cryptoSelect) => {
-
+cryptoSelected.addEventListener('change', (seleccionCrypto) => {
+    var precioCryptomoneda = Cryptos[seleccionCrypto.target.value].precio;
+    document.querySelector("#precioCrypto").value = precioCryptomoneda;
 })
 
+const calcularCompra = () => {
+    let precioCrypto = document.querySelector("#precioCrypto").value;
+    let inversionEnDolares = document.querySelector("#usdCrypto").value;
+    let cantidadComprada = dividir(inversionEnDolares, precioCrypto);
+    document.querySelector("#tuCompra").value = cantidadComprada;
+
+    Cryptos[cryptoSelected.value].compraUsd = inversionEnDolares;
+    Cryptos[cryptoSelected.value].compraDeCrypto = cantidadComprada;
+    console.log(Cryptos[cryptoSelected.value])
+}
 
 
+// C R E A D O R    D E    C A R D S
+
+const agregarPortfolio = () => {
+    const divCrypto = document.createElement("div");
+        divCrypto.classList.add("customCard", "cardShadow");
+
+        const cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
 
 
+        divCrypto.innerHTML += `<div class="card-body">
+                                    <h5 class="card-title">${Cryptos[cryptoSelected.value].nombre}</h5>
+                                    <ul>
+                                        <li>
+                                        precio: ${Cryptos[cryptoSelected.value].precio}
+                                        </li>
+                                        <li>
+                                        tu compra en dolares: ${Cryptos[cryptoSelected.value].compraUsd}
+                                        </li>
+                                        <li>
+                                        tendrias ${Cryptos[cryptoSelected.value].compraDeCrypto} ${Cryptos[cryptoSelected.value].nombre}
+                                        </li>
+                                    </ul>
+                                </div>`
 
+        cardsCryptos.appendChild(divCrypto);
 
-
-
-
-
-
-
-
-
-
-
-/*     let fiatElegida = prompt(`Ingrese su moneda:\n ARS (ingrese 1)\n CLP (ingrese 2) \n BRL (ingrese 3)\n COP (ingrese 4)`),
-    cantidadPesos = parseFloat(prompt(`Ingrese cuantos pesos tiene.`)),
-
-    cantidadDolar = dividir(cantidadPesos, plataFiat[fiatElegida - 1].valorDolar),
-    compraCrypto;
-
-alert(`usted tiene ${cantidadDolar} dolares`);
-
-
-// P R O G R A M A
-
-if (cantidadDolar <= 0) {
-    console.log(`Usted no posee capital de inversión`); // por si no tiene ni 1 dolar para invertir
-} else {
-
-    let inicio = prompt(`Desea calcular cuanto podria comprar de una criptomoneda? escriba "si" o "no"`);
-
-    if (inicio == `no` || inicio == `NO` || inicio == null) {
-        console.log(`Que tenga un buen dia`);
-    } else if (inicio == `si` || inicio == `SI`) {
-
-        do {
-            let cryptoElegida = prompt(`qué criptomoneda desea comprar?:\n Ethereum (ingrese 1)\n Bitcoin (ingrese 2) \n Solana (ingrese 3)\n Cardano (ingrese 4)\n XMR (ingrese 5) \n BNB (Ingrese 6)`);
-
-            compraCryptoUsd = prompt(`Cuantos dolares desea comprar de ${Cryptos[cryptoElegida - 1].nombre}?`);
-
-            Cryptos[cryptoElegida - 1].compraUsd = compraCryptoUsd;
-
-            Cryptos[cryptoElegida - 1].compraDeCrypto = compraCryptoUsd / Cryptos[cryptoElegida - 1].precio; // calcular cuanto va a comprar de la coin
-
-            cantidadDolar = restar(cantidadDolar, compraCryptoUsd); // cuantos dolares le quedan
-
-            console.log(`vas a comprar ${Cryptos[cryptoElegida - 1].compraDeCrypto} de ${Cryptos[cryptoElegida - 1].nombre} que son ${Cryptos[cryptoElegida - 1].compraUsd} dolares`); // confirmacion de lo que va a comprar
-
-            inicio = prompt(`desea comprar otra crypto? ingrese "si" o "no"`);
-
-
-
-
-        } while ((inicio != null) && (inicio != `no`) && (inicio != `NO`)) {
-            console.log(`Te van a quedar ${cantidadDolar} dolares`);
-
-            const compraFinal = Cryptos.filter((crypto) => crypto.compraUsd > 0); // para visualizar todas las compras que va a realizar sin mostrar las que no compra)
-
-            console.log(compraFinal);
-        }
-    }
-} */
+}
